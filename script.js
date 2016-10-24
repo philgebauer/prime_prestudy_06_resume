e292fd03b6eb1cbcddde15db787bb0219ff3769b
Phil G <philg@Phils-MacBook-Pro.local>
2016-10-23 19:48:40 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+     $('#click').on('click', function(){
+     $('#container').fadeIn('slow').css({'display': 'block'});
+       $('#click').css({'display': 'none'})
+  });
+
+});
