d34fdae12a729b6fb2919b04a81c8167ca279d82
philgebauer <gebauer.phil@gmail.com>
2016-10-23 20:42:22 -0500

    Add slideToggle

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
 $(document).ready(function(){
-     $('#click').on('click', function(){
-     $('#container').fadeIn('slow').css({'display': 'block'});
-       $('#click').css({'display': 'none'})
-  });
-
+    $("#click").click(function(){
+    $("#container").slideToggle();
+   });
 });
